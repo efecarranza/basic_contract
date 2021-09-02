db2b4f992467a215633ea2eaf15b602a8d929a06
piscu Carranza <fcarr004@fiu.edu>
2021-09-02 19:41:26 -0300

    add some more tests

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -5,17 +5,30 @@ const { interface, bytecode } = require('../compile');
 
 const web3 = new Web3(ganache.provider());
 
+const initialMessage = 'Hi there!';
 let accounts;
 let inbox;
 beforeEach(async () => {
     accounts = await web3.eth.getAccounts();
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Hi there!'] })
+        .deploy({ data: bytecode, arguments: [initialMessage] })
         .send({ from: accounts[0], gas: '1000000' });
 });
 
 describe('Tests Inbox contract', () => {
     it('deploys a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
+    });
+
+    it('has a default message', async () => {
+        assert.equal(await inbox.methods.message().call(), initialMessage);
+    });
+
+    it('updates the contracts message', async () => {
+        assert.equal(await inbox.methods.message().call(), initialMessage);
+
+        await inbox.methods.setMessage('new message').send({ from: accounts[0]});
+
+        assert.equal(await inbox.methods.message().call(), 'new message');
     });
 });
